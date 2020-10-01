92ec239375ccaa7c667ef2426d6def4978874be2
Peyret Romain <romain.peyret@viacesi.fr>
2020-10-01 17:35:20 +0200

    Created corked code for column and tile

--- src/connectfour/Tile.java
+++ src/connectfour/Tile.java
@@ -1,5 +1,29 @@
 package connectfour;
 
 public class Tile {
+	private Token token;
 
+	public Token getToken() {
+		return token;
+	}
+
+	public void setToken(Token token) {
+		this.token = token;
+	}
+	
+	public boolean isEmpty() {
+		if(this.getToken() instanceof Token) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public String toString() {
+		if(!this.isEmpty()) {
+			return "" + this.getToken().getSymbol();
+		} else {
+			return "";
+		}
+	}
 }
